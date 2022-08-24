3f021e601b3e45af0c4b9e3078673baba73e7e0b
Kevin Cubas <kevin.otaku@hotmail.com>
2022-08-24 02:08:49 -0600

    Avances en componentes de paises

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -6,11 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent {
 
-  constructor() { }
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
 
-  ngOnInit(): void {
+  constructor( private paisService: PaisService ) { }
+
+  buscar() {
+    this.hayError = false;
+    console.log(this.termino)
+    
+    this.paisService.buscarPais(this.termino)
+      .subscribe( paises => {
+        console.log(paises);
+        this.paises = paises;
+
+      }, (err) => {
+        this.hayError = true;
+        this.paises = [];
+      } );
   }
 
 }
