a01f24dbd7a2ed15b1a2d23285a1d325c296d212
Kevin Cubas <kevin.otaku@hotmail.com>
2022-08-25 17:33:15 -0600

    sección 9 terminado

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
@@ -8,7 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorRegionComponent implements OnInit {
 
-  constructor() { }
+  regiones: string[] = ['EU', 'SAARC', 'ASEAN', 'CAIS', 'NAFTA'];
+  regionActiva: string = '';
+  paises: Country[] = [];
+
+  constructor( private paisService: PaisService ) { }
+
+  getClaseCSS( region: string ): string {
+    return (region === this.regionActiva ? 'btn btn-primary' : 'btn btn-outline-primary')
+  }
+
+  activarRegion(region: string) {
+    if(region === this.regionActiva) {return;}
+    this.regionActiva = region;
+    this.paisService.buscarRegion(region)
+      .subscribe(paises => {
+        this.paises = paises;
+      })
+
+  }
 
   ngOnInit(): void {
   }
